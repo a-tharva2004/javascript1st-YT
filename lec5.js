1c06f84cfbf429dc4f7d5cf6cb4d47febf57311b
a-tharva2004 <150817653+a-tharva2004@users.noreply.github.com>
2024-04-26 12:25:47 +0000

    lec5done

---
+++ lec5.js
@@ -0,0 +1,10 @@
+"use strict" //used for newer version
+let age =5
+
+let accId=null
+
+
+//alert//we are using node js not browser
+console.log(accId)
+console.log(typeof undefined)//o/p=undefined
+console.log(typeof null)//o/p=object
